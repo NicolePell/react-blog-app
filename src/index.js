d326122830d1dac39a75e163116491a9d4fa5e36
NicolePell <pellicena@gmail.com>
2017-03-13 12:27:32 +0000

    Add call to fetch posts and render on IndexRoute

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
 import { Router, browserHistory } from 'react-router'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
 
-import routes from './routes'
 import reducers from './reducers'
+import routes from './routes'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
